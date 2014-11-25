3f70a7e8456560b8fcb45b5b0bb3d674581ce5a6
gytis <gytis@redhat.com>
2014-11-25 11:53:43 +0000

    Use styles and js provided by static.jboss.org

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,20 +1,14 @@
-require 'wget_wrapper'
 require 'js_minifier'
 require 'css_minifier'
 require 'html_minifier'
-require 'file_merger'
-require 'less_config'
 require 'breadcrumb'
 
 Awestruct::Extensions::Pipeline.new do
   helper Awestruct::Extensions::Partial
   helper Awestruct::Extensions::Breadcrumb
-  extension Awestruct::Extensions::WgetWrapper.new
   transformer Awestruct::Extensions::JsMinifier.new
   transformer Awestruct::Extensions::CssMinifier.new
   transformer Awestruct::Extensions::HtmlMinifier.new
-  extension Awestruct::Extensions::FileMerger.new
-  extension Awestruct::Extensions::LessConfig.new
   helper Awestruct::Extensions::GoogleAnalytics
   extension Awestruct::Extensions::Indexifier.new([/^\/docs\/.*/, /\/404.html/]) # Exclude generated docs from "Indexification"
 end
